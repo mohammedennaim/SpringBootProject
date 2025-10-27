82bfcd3690e29c7a430a5ab39de41a6046fd6c2d
mohammedennaim <mohammedennaim99@gmail.com>
2025-10-27 14:17:52 +0100

    create structure de projet

---
+++ src/test/java/com/example/digitallogistics/DigitalLogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitallogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
