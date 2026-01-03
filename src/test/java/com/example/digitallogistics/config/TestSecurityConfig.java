95724bd7a760745f76bad5d4395a72355f581181
mohammedennaim <mohammedennaim99@gmail.com>
2026-01-03 17:01:32 +0100

    feat(security): ajouter un gestionnaire d'authentification pour les tests avec JWT

--- src/test/java/com/example/digitallogistics/config/TestSecurityConfig.java
+++ src/test/java/com/example/digitallogistics/config/TestSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,6 +33,12 @@ import java.util.Map;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class TestSecurityConfig {
+
+    @Bean
+    @Primary
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     @Primary
